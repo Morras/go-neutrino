7022b89ba126b34521d280107e1d2ec015808d89
Morras <M.Rasmussen84@gmail.com>
2015-09-12 14:05:28 +0200

    Refactored game controller into a class to avoid global variables. Moved state into the game model so a game is now a selfcontained entity that can keep track if its state. Other minor fixes (I should work on my commit skills)

--- game-models.go
+++ game-models.go
@@ -2,6 +2,8 @@ package neutrino
 
 import "fmt"
 
+//// Move type ////
+
 type Move struct {
 	FromX, FromY, ToX, ToY byte
 }
@@ -15,6 +17,8 @@ func NewMove(fromX, fromY, toX, toY byte) Move {
 	}
 }
 
+//// State type ////
+
 type State byte
 
 const (
@@ -26,34 +30,51 @@ const (
 	Player2Win State = 5
 )
 
-type entry byte
+//// entry type ////
+
+type Entry byte
 
 const (
-	EmptySquare = '0'
-	Player1 entry = '1'
-	Player2 = '2'
-	Neutrino = 'N'
+	EmptySquare Entry = '0'
+	Player1 Entry = '1'
+	Player2 Entry = '2'
+	Neutrino Entry = 'N'
 )
 
+//// Game type ////
+
 type Game struct {
-	game [25]entry
+	game [25]Entry
+	State State
 }
 
 const (
 	widthOfBoard = 5;
 )
 
-func NewGame() *Game{
+func NewEmptyGame() *Game{
 	game := &Game{}
+	for x := byte(0); x < 5; x++ {
+		for y := byte(0); y < 5; y++ {
+			game.SetLocation(x, y, EmptySquare)
+		}
+	}
+	game.State = Player1NeutrinoMove
+	return game;
+}
+
+func NewStandardGame() *Game{
+	game := NewEmptyGame()
 	for i := byte(0); i < 5; i++ {
 		game.SetLocation(i, 0, Player1)
 		game.SetLocation(i, 4, Player2)
 	}
 	game.SetLocation(2, 2, Neutrino)
+	game.State = Player1NeutrinoMove
 	return game
 }
 
-func (self *Game) SetLocation(x, y byte, entry entry) error{
+func (self *Game) SetLocation(x, y byte, entry Entry) error{
 	if x > 4 || y > 4{
 		return fmt.Errorf("Coordinates must be between (0,0) and (4,4) both inclusive. Was (%d, %d)", x, y)
 	}
@@ -61,9 +82,23 @@ func (self *Game) SetLocation(x, y byte, entry entry) error{
 	return nil
 }
 
-func (self *Game) GetLocation(x, y byte) (entry, error){
+func (self *Game) GetLocation(x, y byte) (Entry, error){
 	if x > 4 || y > 4{
 		return 9, fmt.Errorf("Coordinates must be between (0,0) and (4,4) both inclusive. Was (%d, %d)", x, y)
 	}
 	return self.game[x+widthOfBoard*y], nil
+}
+
+/**
+ * This has proven usefull for debugging.
+ * Note that the entries are the
+ * ascii value of the rune, so 48 = '0'
+ */
+func (self *Game) PrintGame(){
+	for y := 0; y < 5; y++ {
+		for x := 0; x < 5; x++ {
+			fmt.Printf("%f", self.game[x+widthOfBoard*y])
+		}
+		fmt.Println()
+	}
 }
--- game.go
+++ game.go
@@ -1,26 +1,91 @@
 package neutrino
 
-var game *Game
-var state State
+import "fmt"
 
-var moveChannel chan Move
-var stateChannel chan State
+type GameController struct{
+	game *Game
 
-func StartGame(g *Game) (<-chan Move, <-chan State){
-	game = g
-	moveChannel = make(chan Move)
-	stateChannel = make(chan State)
-	return moveChannel, stateChannel
+	moveChannel chan Move
+	stateChannel chan State
 }
 
-func MakeMove(move Move) error{
-	moveChannel <- move
-	state++
-	stateChannel <- state
-	return nil
+func (self *GameController) StartGame(g *Game) (<-chan Move, <-chan State) {
+	self.game = g
+	self.moveChannel = make(chan Move)
+	self.stateChannel = make(chan State)
+	return self.moveChannel, self.stateChannel
 }
 
-func EndGame(){
-	close(moveChannel)
-	close(stateChannel)
+func (self *GameController) MakeMove(m Move) (State, error) {
+	legalMove, errorMessage := self.isMoveLegal(m)
+	if !legalMove {
+		return self.game.State, fmt.Errorf(errorMessage)
+	}
+
+	self.move(m)
+
+	winnerExists, winnerState := self.isThereAWinner()
+	if winnerExists {
+		self.stateChannel <- winnerState
+		return winnerState, nil
+	}
+
+	self.game.State = self.getNextState()
+	self.stateChannel <- self.game.State
+	return self.game.State, nil
+}
+
+func (self *GameController) EndGame() {
+	close(self.moveChannel)
+	close(self.stateChannel)
+}
+
+func (self *GameController) isMoveLegal(move Move) (bool, string) {
+	//TODO
+	return true, "no error"
+}
+
+func (self *GameController) move(move Move) {
+	var newEntry Entry
+
+	switch self.game.State {
+	case Player1NeutrinoMove:
+		newEntry = Neutrino
+	case Player2NeutrinoMove:
+		newEntry = Neutrino
+	case Player1Move:
+		newEntry = Player1
+	case Player2Move:
+		newEntry = Player2
+	default:
+		panic(fmt.Sprintf("Unable to perform move at the current state %d", self.game.State))
+	}
+
+	self.game.SetLocation(move.FromX, move.FromY, EmptySquare)
+	self.game.SetLocation(move.ToX, move.ToY, newEntry)
+	self.moveChannel <- move
+}
+
+func (self *GameController) isThereAWinner() (bool, State) {
+	//TODO
+	//See if neutrino is on back row
+	//See if neutrino is on front row
+	//See if neutrino is blocked
+	//Calculate who is winner if it is blocked
+	return false, self.game.State
+}
+
+func (self *GameController) getNextState() State {
+	switch self.game.State {
+	case Player1NeutrinoMove:
+		return Player1Move
+	case Player1Move:
+		return Player2NeutrinoMove
+	case Player2NeutrinoMove:
+		return Player2Move
+	case Player2Move:
+		return Player1NeutrinoMove
+	}
+	//We should never get here
+	panic(fmt.Sprintf("Game is in a state it cannot move on from %d", self.game.State))
 }
